d8184ed4cb3c2b28305cfb731c7b6ed14fcfa938
Hamzahzahh09 <hamzzahh09@gmail.com>
2025-05-15 11:43:15 +0700

    perpustakaan

---
+++ src/components/Library/LibraryOnline.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="font-bold text-2xl text-center">Selamat datang di perpus</h1>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import LibraryOnline from './components/Library/LibraryOnline.vue'
+
+createApp(LibraryOnline).mount('#app')
